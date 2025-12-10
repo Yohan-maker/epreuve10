38d2b7a3c44308850be64dfcfce2a6fe1641d563
Yohan-maker <yohan1.aline@gmail.com>
2025-12-09 21:43:03 -0400

    Add files via upload

---
+++ ignite.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import time
+
+print("\n[IEM] Initialisation IEM...")  # IEM = Impulsion ÉlectroMagnétique
+time.sleep(1)
+print("[IEM] Impulsion en cours...")
+time.sleep(1)
+print("[IEM] L.U.M.E.N.  INTERRUPTION DU SIGNAL")
+time.sleep(1)
+
+print("""
+________________________________________
+|                                      |
+|    ____   _____   _____              |
+|   /  _ \ |  ___| |  ___|             |
+|   | | | || |__   | |__               |
+|   | | | ||  __|  |  __|              |
+|   | |_| || |     | |                 |
+|   \____/ |_|     |_|                 |
+|                                      |
+|  L'U.M.E.N. EST OFF.                 |
+|  L'UNIVERSITE EST SAUVEE.            |
+|                                      |
+|  FLAG FINAL : FLAG-LUMEN-10          |
+|                                      |
+|______________________________________|
+""")
---
+++ prepare.py
@@ -0,0 +1,3 @@
+print("[IEM] Chargement du noyau de désactivation...")
+print("[IEM] Vérification de l’intégrité des modules...")
+print("[IEM] Prêt pour l’activation finale.")
